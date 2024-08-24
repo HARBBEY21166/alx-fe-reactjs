9e49c1f2dd9fc983e3485fd54dcba90345225bc0
ABBEY AINA <olamilekansunday445@gmail.com>
2024-08-24 20:31:14 +0200

    Update Footer.jsx

--- alx-react-app-new/src/components/Footer.jsx
+++ alx-react-app-new/src/components/Footer.jsx
@@ -1,9 +1,11 @@
-function Footer() {
+import React from 'react';
+
+const Footer = () => {
   return (
-      <footer>
-          <p>&copy; 2023 City Lovers</p>
-      </footer>
+    <footer style={{ backgroundColor: 'darkgray', color: 'white', padding: '10px', textAlign: 'center' }}>
+       <p>&copy; 2023 City Lovers</p>
+    </footer>
   );
-}
+};
 
 export default Footer;
