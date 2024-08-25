a71c76eb98cecefe928d302f3961cec6ac610bf9
ABBEY AINA <olamilekansunday445@gmail.com>
2024-08-25 23:26:00 +0200

    Update RecipeList.jsx

--- recipe-sharing-app/src/components/RecipeList.jsx
+++ recipe-sharing-app/src/components/RecipeList.jsx
@@ -1,8 +1,10 @@
 import React from 'react';
 import { useRecipeStore } from '../recipeStore';
+import { useRecipeStore } from './recipeStore';
 
 const RecipeList = () => {
   const filteredRecipes = useRecipeStore(state => state.filteredRecipes);
+  const { recipes } = useRecipeStore(state => state);
 
   return (
     <div>
