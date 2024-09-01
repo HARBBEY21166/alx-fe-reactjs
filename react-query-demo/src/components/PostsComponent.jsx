757099a3ec2f7991c53978d689a1aef8c84ff4e4
ABBEY AINA <olamilekansunday445@gmail.com>
2024-09-01 18:50:45 +0200

    Update PostsComponent.jsx

--- react-query-demo/src/components/PostsComponent.jsx
+++ react-query-demo/src/components/PostsComponent.jsx
@@ -1,3 +1,4 @@
+// src/PostsComponent.jsx
 import React from 'react';
 import { useQuery, useQueryClient } from 'react-query';
 import axios from 'axios';
@@ -14,6 +15,9 @@ const PostsComponent = () => {
     {
       // caching settings
       staleTime: 10000, // cache for 10 seconds
+      cacheTime: 30000, // cache for 30 seconds
+      refetchOnWindowFocus: true, // refetch on window focus
+      keepPreviousData: true, // keep previous data while refetching
     }
   );
 
