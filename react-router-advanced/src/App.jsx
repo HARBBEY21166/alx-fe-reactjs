db035e62891141ae7c30693f3afcb235263b5064
ABBEY AINA <olamilekansunday445@gmail.com>
2024-09-01 19:16:07 +0200

    Update App.jsx

--- react-router-advanced/src/App.jsx
+++ react-router-advanced/src/App.jsx
@@ -1,7 +1,10 @@
 import React from 'react';
-import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import { BrowserRouter, Route, Switch, Routes } from 'react-router-dom';
 import Profile from './Profile';
 import ProtectedRoute from './ProtectedRoute';
+import Login from './Login';
+import UserProfile from './UserProfile';
+import BlogPost from './BlogPost';
 
 const App = () => {
   return (
