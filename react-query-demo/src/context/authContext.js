ac444008170259e8578e83e279ffa9d5b3c4dbc2
ABBEY AINA <olamilekansunday445@gmail.com>
2024-09-01 15:30:06 +0200

    Create authContext.js

---
+++ react-query-demo/src/context/authContext.js
@@ -0,0 +1,27 @@
+// src/context/authContext.js
+import { createContext, useState } from 'react';
+
+const AuthContext = createContext();
+
+const AuthProvider = ({ children }) => {
+  const [user, setUser] = useState(null);
+  const [token, setToken] = useState(null);
+
+  const login = (user, token) => {
+    setUser(user);
+    setToken(token);
+  };
+
+  const logout = () => {
+    setUser(null);
+    setToken(null);
+  };
+
+  return (
+    <AuthContext.Provider value={{ user, token, login, logout }}>
+      {children}
+    </AuthContext.Provider>
+  );
+};
+
+export { AuthProvider, AuthContext };
