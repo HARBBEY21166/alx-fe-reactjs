f66e503d2fbd8fa3a98a1fe6e2f3f27671d0b7ac
ABBEY AINA <olamilekansunday445@gmail.com>
2024-09-01 16:20:56 +0200

    Update index.js

--- react-router-advanced/src/index.js
+++ react-router-advanced/src/index.js
@@ -4,13 +4,14 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import Login from './Login';
 import Dashboard from './Dashboard';
+import PrivateRoute from './PrivateRoute';
 
 ReactDOM.render(
   <React.StrictMode>
     <BrowserRouter>
       <Switch>
         <Route path="/" exact component={Login} />
-        <Route path="/dashboard" component={Dashboard} />
+        <PrivateRoute path="/dashboard" component={Dashboard} />
       </Switch>
     </BrowserRouter>
   </React.StrictMode>,
