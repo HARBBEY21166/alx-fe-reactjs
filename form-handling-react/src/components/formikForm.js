d960dfb2cb0f0057d7f49f70cc70373a416106e1
ABBEY AINA <olamilekansunday445@gmail.com>
2024-09-01 14:21:22 +0200

    Update formikForm.js

--- form-handling-react/src/components/formikForm.js
+++ form-handling-react/src/components/formikForm.js
@@ -1,48 +1,45 @@
+// src/components/formikForm.js
 import React from 'react';
 import { Formik, Form, Field, ErrorMessage } from 'formik';
 import * as Yup from 'yup';
 
-const RegistrationForm = () => {
-  const validationSchema = Yup.object().shape({
-    username: Yup.string().required('Required'),
-    email: Yup.string()
-      .email('Invalid email format')
-      .required('Required'),
-    password: Yup.string()
-      .min(6, 'Password must be at least 6 characters')
-      .required('Required'),
+const formikForm = () => {
+  const formikSchema = Yup.object().shape({
+    username: Yup.string().required('Username is required'),
+    email: Yup.string().email('Invalid email').required('Email is required'),
+    password: Yup.string().required('Password is required'),
   });
 
   return (
     <Formik
       initialValues={{ username: '', email: '', password: '' }}
-      validationSchema={validationSchema}
+      validationSchema={formikSchema}
       onSubmit={(values, { setSubmitting }) => {
-        setTimeout(() => {
-          // You can send the form data to your backend here
-          console.log('Form submitted:', values);
-          setSubmitting(false);
-        }, 500);
+        console.log('Form submitted successfully!', values);
+        setSubmitting(false);
       }}
     >
-      {({ isSubmitting }) => (
+      {({ isSubmitting, isValid }) => (
         <Form>
-          <div>
-            <label htmlFor="username">Username:</label>
-            <Field type="text" id="username" name="username" />
-            <ErrorMessage name="username" />
-          </div>
-          <div>
-            <label htmlFor="email">Email:</label>
-            <Field type="email" id="email" name="email" />
-            <ErrorMessage name="email" />
-          </div>
-          <div>
-            <label htmlFor="password">Password:</label>
-            <Field type="password" id="password" name="password" />
-            <ErrorMessage name="password" />
-          </div>
-          <button type="submit" disabled={isSubmitting}>
+          <label>
+            Username:
+            <Field type="text" name="username" />
+            <ErrorMessage name="username" component="div" style={{ color: 'red' }} />
+          </label>
+          <br />
+          <label>
+            Email:
+            <Field type="email" name="email" />
+            <ErrorMessage name="email" component="div" style={{ color: 'red' }} />
+          </label>
+          <br />
+          <label>
+            Password:
+            <Field type="password" name="password" />
+            <ErrorMessage name="password" component="div" style={{ color: 'red' }} />
+          </label>
+          <br />
+          <button type="submit" disabled={isSubmitting || !isValid}>
             Submit
           </button>
         </Form>
@@ -51,4 +48,4 @@ const RegistrationForm = () => {
   );
 };
 
-export default RegistrationForm;
+export default formikForm;
